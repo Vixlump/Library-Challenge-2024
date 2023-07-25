39aa2adcf5a6d5cda466ab62c1c8fafeb6b5a80d
Vixlump <17090765+Vixlump@users.noreply.github.com>
2023-07-24 23:17:11 -0600

    string math library for C++
    might not convert this one to C and python it was a bit of a pain and very long

---
+++ c++_string_math_library/compile_example.sh
@@ -0,0 +1,1 @@
+g++ example_usage.cpp -o EXAMPLE_BINARY_STR_MATH -std=c++20 -pedantic -O2
---
+++ c++_string_math_library/example_usage.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <string>
+
+#include "string_math_library.hpp"
+
+int main() {
+    std::string num1, num2;
+    std::cout << "Enter two positive integer strings: ";
+    std::cin >> num1 >> num2;
+
+    //Addition
+    std::string sum = vix::add_strings(num1, num2);
+    std::cout << "Added Strings: " << sum << std::endl;
+
+    //Subtraction
+    std::string diff = vix::subtract_strings(num1, num2);
+    std::cout << "Subtracted Strings: " << diff << std::endl;
+
+    //Multiplication
+    std::string product = vix::multiply_strings(num1, num2);
+    std::cout << "Multiplied Strings: " << product << std::endl;
+
+    //Division
+    std::string quotient, remainder;
+    quotient = vix::divide_strings(num1, num2, remainder);
+    std::cout << "Divided Strings: " << quotient << ", Strings Remainder: " << remainder << std::endl;
+
+    return 0;
+}
+
